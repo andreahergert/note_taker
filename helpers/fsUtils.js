1d6f8b503dbd80e11f3cb07a66b7966a5cf60377
Andrea Hergert <ahergert24@hotmail.com>
2022-10-27 16:34:57 -0700

    added helpers folder for fsutil and uuid code

---
+++ helpers/fsUtils.js
@@ -0,0 +1,37 @@
+const fs = require("fs");
+const util = require("util");
+
+const readFromFile = util.promisify(fs.readFile);
+
+const writeToFile = (destination, content) =>
+    fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+        err ? console.error(err) : console.info(`\nData written to ${destination}`)
+    );
+
+const readAndAppend = (content, file) => {
+    fs.readFile(file, "utf8", (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            const parsedData = JSON.parse(data);
+            parsedData.push(content);
+            writeToFile(file, parsedData);
+        }
+    });
+};
+
+const readAndDelete = (id, file) => {
+    fs.readFile(file, "utf8", (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            const parsedData = JSON.parse(data);
+            let filteredData = parsedData.filter((note) => {
+                return note.id != id;
+            });
+            writeToFile(file, filteredData);
+        }
+    });
+};
+
+module.exports = { readFromFile, writeToFile, readAndAppend, readAndDelete };
