e506d5f7d383fbc7fa46decadf41805103c630c1
Andrea Hergert <ahergert24@hotmail.com>
2022-10-28 15:15:08 -0700

    added wildcard for index html

--- server.js
+++ server.js
@@ -18,6 +18,10 @@ app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
 
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+);
+
 app.listen(PORT, () => {
   console.log(`App listening at http://localhost:${PORT} 🚀`);
 });
